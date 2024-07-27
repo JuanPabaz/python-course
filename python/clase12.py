f28b727508ea92d2afea63944f04d42b6a7b3e05
JuanPabaz <jpgthf@gmail.com>
2024-07-27 13:37:34 -0500

    feat: class 12 implement if's

---
+++ python/clase12.py
@@ -0,0 +1,16 @@
+x = 5
+y = 10
+if x > 5:
+    print("X es mayor que 5")
+elif x == 5:
+    print("X es igual que 5")
+else:
+    print("X es menor que 5")
+
+if x > 5 and y > 10:
+    print("X es mayor que 5 y Y es mayor que 10")
+else:
+    print("No se")
+
+if not x > 5:
+    print("X no es mayor que 5")
