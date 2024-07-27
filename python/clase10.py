4a80fda80b9b5d7311bf9c21d007c4b423333510
JuanPabaz <jpgthf@gmail.com>
2024-07-27 11:59:11 -0500

    feat: class 10 tuples and matrix

---
+++ python/clase10.py
@@ -0,0 +1,10 @@
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(matrix)
+print(matrix[0][2])
+
+numbers = (1,2,3,4,5)
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+numbers.__add__((6,6))
+print(numbers)
