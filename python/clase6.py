6e157a2f26b1635e4101800f22810101e815fb6d
JuanPabaz <jpgthf@gmail.com>
2024-07-27 11:28:54 -0500

    feat: class 6 operations

---
+++ python/clase6.py
@@ -0,0 +1,25 @@
+a = 10
+b = 3
+print("Suma:",a+b)
+print("Resta:",a-b)
+print("Multiplicacion:",a*b)
+print("Potenciacion:",a**b)
+print("Division:", a/b)
+print("Division parte entera:", a//b)
+
+print("Modulo",a%b)
+
+#a = a + 2
+a +=2
+print(a)
+#a = a -2
+a -=2
+#a = a * 2
+a *=2
+#a = a/2
+a /=2
+
+a = 10
+b = 3
+print(a<b)#Devuelve un booleano de si a es menor que b
+print(a>b)#Devuelve un booleano de si a es mayor que b
