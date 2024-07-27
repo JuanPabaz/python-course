8fbd3de0fdf401c416ddd21803b972cfd5b2a41a
JuanPabaz <jpgthf@gmail.com>
2024-07-27 11:33:34 -0500

    feat: class 7 inputs on console

---
+++ python/clase7.py
@@ -0,0 +1,7 @@
+#El resultado de una variable por input siempre sera un String
+nombre = input("Ingrese su nombre: ")
+print(nombre)
+print(type(nombre))
+edad = int(input("Ingrese su edad: "))#Castear un dato
+print(edad)
+print(type(edad))
