b43e9b7511e5240fdfb699bd7cb38bd93c5ced47
JuanPabaz <jpgthf@gmail.com>
2024-07-27 10:48:35 -0500

    feat: class 3 variable management

---
+++ python/clase3.py
@@ -0,0 +1,11 @@
+
+#No se puede empezar una variable con un numero, caracter especial, solo con letras 
+#y con guion bajo (_nombre_).
+#Python es un lenguaje ordenado, ejecuta de arriba a abajo.
+#Palabras reservadas = class
+saludo = "Hola"
+saludo2 = "Hola 2"
+nombre = "Juan Pablo"
+
+print(saludo)
+print(nombre)
