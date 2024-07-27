89118e6579c2c3835f84bf55fc2a8d7eb328476e
JuanPabaz <jpgthf@gmail.com>
2024-07-27 11:15:30 -0500

    feat: class 5 integer, float and boolean management

---
+++ python/clase5.py
@@ -0,0 +1,15 @@
+x = 10 #Entero
+y = 5.25 #Decimal o Float
+# z = 1.2e6 #Float
+# a = 1.2e-3 #Float
+print(type(x))
+print(type(y))
+
+#Operaciones
+print(x+y)
+
+#Boolean
+is_true = True
+is_false = False
+print(is_true)
+print(is_false)
